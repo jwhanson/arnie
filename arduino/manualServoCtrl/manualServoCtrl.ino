c1d69c6056f0df15887d23b6b61e03754244cd9e
Kirk Boyd <kirkland.boyd@trincoll.edu>
2022-04-14 14:58:53 -0600

    finalize comments on manualServoCtrl

--- arduino/manualServoCtrl/manualServoCtrl.ino
+++ arduino/manualServoCtrl/manualServoCtrl.ino
@@ -1,3 +1,15 @@
+/* manualServoCtrl.ino
+ *
+ * Author:
+ * Kirk Boyd
+ * 
+ *
+ * Description:
+ * This is just a debug sketch to run on the DFRobot Romeo board which controls
+ * the LeArm. It allows manual control of the servos in the arm just for positioning
+ * and testing. This uses an old control scheme, however and is probably no longer useful.
+ */
+
 #include <Servo.h>
 #include <Ramp.h>
 #define servo0pin 3
@@ -9,17 +21,13 @@
 #define relay3 12
 #define relay4 13
 
-
+// create servo objects
 Servo servo0;
 Servo servo1;
 Servo servo2;
 Servo servo3;
 
-ramp ramp0;
-ramp ramp1;
-ramp ramp2;
-ramp ramp3;
-
+// stepping variables
 int step0;
 int step1;
 int step2;
@@ -38,18 +46,15 @@ int d2;
 int d3;
 int servo3level;
 
-int getCup[] = {0,40,180,40};
+int getCup[] = {0,40,180,40}; // array to store a good 
+// position for receiving the cup from the user. Also deprecated.
 
-long t = 2500;
-int dT = 50;
+long t = 2500; // time for each motion to be completed
+int dT = 50; // time step
 int N; // = 50; //t/dT;
-
-
-
 int pos = 0;    // variable to store the servo position
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(relay1,OUTPUT);
   pinMode(relay2,OUTPUT);
@@ -63,19 +68,20 @@ void setup() {
   digitalWrite(relay2,LOW);
   digitalWrite(relay3,LOW);
   digitalWrite(relay4,LOW);
-//  home();
+  home();
   Serial.println("Home");
   delay(1500);
-//  receive_cute();
-//  pulled_up_cntrd();
-//  fill_cute();
-//  moveLvl(0,40,180,40);
-//  moveLvl(90,135,135,170);
-//  moveLvl(180,40,180,40);
-//  moveLvl(180,50,90,60);
+}
+
+
+
+void loop() {
+  servo0.write(0);
+//  delay(2000);
 }
 
 void moveLvl(int gol0, int gol1, int gol2, int gol3){  
+  // this program i
   N = t/dT;
   pos0 = servo0.read(); //90;
   pos1 = servo1.read(); //90;
@@ -89,26 +95,19 @@ void moveLvl(int gol0, int gol1, int gol2, int gol3){
   step1 = d1/N;
   step2 = d2/N;
   step3 = d3/N;
-//  while(
     for (int i=0; i<N; i++){
       servo0.write(i*step0 + pos0);
       servo3level = (i*step1+pos1) + (180 - (i*step2+pos2));
       servo3.write(servo3level);
       servo1.write(i*step1 + pos1);
       servo2.write(i*step2 + pos2);
-//      Serial.print("joint0 desired: ");
-//      Serial.print(i*step0 + pos0);
-//      Serial.print("\t | joint1 desired: ");
-//      Serial.print(i*step1 + pos1);
-//      Serial.print("\t | joint2 desired: ");
-//      Serial.print(i*step2 + pos2);
-//      Serial.print("\t | joint3 desired: ");
-//      Serial.println(servo3level);
       delay(dT);
     }
 }
 
 void sweep(int goal){
+  // this function is an adaptation of the Arduino Servo.h library example "sweep".
+  // it just runs through every position on every servo in a loop.
     for (pos = 0; pos <= goal; pos += 1) { // goes from 0 degrees to 180deg in steps of 1deg
     servo0.write(pos);  // tell servo to go to position in variable 'pos'
     servo1.write(pos);       
@@ -118,56 +117,9 @@ void sweep(int goal){
     }// waits 15 ms for the servo to reach the position
 }
 
-
-
-void loop() {
-  servo0.write(0);
-//  delay(2000);
-//  servo0.write(90);
-//  delay(2000);
-//  servo0.write(180);
-//  delay(2000);
-//  servo0.write(90);
-//  delay(2000);
-//  servo0.write(180);
-//  moveLvl(0,40,180,40);
-//  Serial.print("joint0 reading: ");
-//  Serial.print(servo0.read());
-//  Serial.print("\t | joint1 reading: ");
-//  Serial.print(servo1.read());
-//  Serial.print("\t | joint2 reading: ");
-//  Serial.print(servo2.read());
-//  Serial.print("\t | joint3 reading: ");
-//  Serial.println(servo3.read());
-//  moveLvl(180,40,180,40);
-//  digitalWrite(relay1,LOW);
-//  digitalWrite(relay2,LOW);
-//  digitalWrite(relay3,LOW);
-//  digitalWrite(relay4,LOW);
-//  delay(1000);
-//  digitalWrite(relay1,HIGH);
-//  digitalWrite(relay2,HIGH);
-//  digitalWrite(relay3,HIGH);
-//  digitalWrite(relay4,HIGH);
-//  delay(1000);
-//  relayPulse();
-
-}
-
-void relayPulse(){
-  digitalWrite(relay1,LOW);
-  digitalWrite(relay2,LOW);
-  digitalWrite(relay3,LOW);
-  digitalWrite(relay4,LOW);
-  delay(5000);
-  digitalWrite(relay1,HIGH);
-  digitalWrite(relay2,HIGH);
-  digitalWrite(relay3,HIGH);
-  digitalWrite(relay4,HIGH);
-  delay(5000);
-}
-
 void printServos(){
+  // this was for very early debugging to read the pots in the servos.
+  // it was not very reliable so this function is probably not worth using.
 //      Serial.print("joint0 desired: ");
 //      Serial.print(i*step0 + pos0);
 //      Serial.print("\t | joint1 desired: ");
@@ -185,4 +137,3 @@ void printServos(){
       Serial.print("\t | joint3 reading: ");
       Serial.println(servo3.read());
 }
-/* GRAVEYARD */
